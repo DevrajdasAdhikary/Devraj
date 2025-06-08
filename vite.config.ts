878dfb58b4e26d5de78b5c1b95185e5b06e85c6c
Devraj Das Adhikary <83987242+DevrajdasAdhikary@users.noreply.github.com>
2025-06-09 05:13:41 +0530

    Update GitHub configuration for DevrajdasAdhikary/Devraj

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/devraj-portfolio/', // Replace with your repository name
+  base: '/Devraj/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
